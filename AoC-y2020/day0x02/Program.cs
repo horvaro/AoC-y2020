9a49bdaa97ef48c52b64dd61a027ab14bd8037e1
Robert Horvath <robert.horvath@fis-he.ch>
2021-10-20 13:27:30 +0200

    Init commit

---
+++ AoC-y2020/day0x02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day0x02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
